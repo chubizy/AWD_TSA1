400bb63a3f5abeb2ec0faaea4b3eb62fa6c7aa6b
chubizy <202310895@fit.edu.ph>
2025-02-28 02:10:55 +0800

    update color

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 document.getElementById('colorButton').addEventListener('click', function() {
-    const colors = ['#167a85', '#084a5e', '#f7c254', '#d43832'];
+    const colors = ['#167a85', '#ead151', '#fc6c87', '#084a5e', '#eab76a',  '#aa4064', '#f7c254', '#f9827a', '#d43832', '#c9b25f'];
     const randomColor = colors[Math.floor(Math.random() * colors.length)];
     document.querySelector('.resume').style.backgroundColor = randomColor;
 });
